d9297f27f1ab80d6caff3bdf6dbea297712c14fd
Conor Sullivan <conor@conorsullivan.net>
2026-08-14 00:06:36 -0500

    implement debounce and clicking

--- just_menu/index.js
+++ just_menu/index.js
@@ -1,7 +1,8 @@
-//Get boxes
 let boxes = document.getElementsByClassName("box");
 
-
+const menuTimeoutMS = 225; //timeout 25 ms < animation duration in css
+let disableControls = false;
+let activeIdx = 2;
 const boxWidth = boxes[0].offsetWidth;
 const boxHeight = boxes[0].offsetHeight;
 let positions = [];
@@ -33,12 +34,13 @@ function calculateInvisibleTop(idx,height,offset) {
 	}
 }
 
-//calculate box positions
+//calculate box positions, add click listener, constant idx
 for(let i = 0; i < boxes.length; i++) {
     let box = boxes[i];
-		
+		box.setAttribute("data-idx",i);
+		box.addEventListener("click", animate);
 		if(i < (boxes.length / 2)){
-			let leftValue = calculateVisibleLeft(i,medianVisIdx,10,endVisIdx);
+			let leftValue = calculateVisibleLeft(i,medianVisIdx,20,endVisIdx);
 			let topValue = calculateVisibleTop(i,boxHeight,10);
    	 	box.style.top = topValue + "px";
 			box.style.left = leftValue + "px";
@@ -51,7 +53,7 @@ for(let i = 0; i < boxes.length; i++) {
 		}
 }
 
-function shiftPositions() {
+function shiftPositionsDown() {
     let tmpArr = Array(boxes.length);
 
     for(let i = 0; i < boxes.length; i++) {
@@ -70,9 +72,67 @@ function shiftPositions() {
     boxes = tmpArr;
 }
 
-document.addEventListener("keydown", function(e) {
-    if(e.key === "ArrowDown") {
-	shiftPositions();
-    }
-})
+function shiftPositionsUp() {
+	let tmpArr = Array(boxes.length);
+	for(let i = boxes.length - 1; i >= 0; i--){
+		let box = boxes[i];
+		if(i === 0){
+			box.style.top = positions[boxes.length-1][1] + "px";
+			box.style.left = positions[boxes.length-1][0] + "px";
+			tmpArr[boxes.length-1] = box;
+		} else {
+			box.style.top = positions[i-1][1] + "px";
+			box.style.left = positions[i-1][0] + "px";
+			tmpArr[i-1] = box;
+		}
+	}
+	boxes = tmpArr;
+}
 
+//need to not animate to hidden half, other than that it's working
+function animateClickSteps(remainingSteps) {
+	if(remainingSteps > 0){
+		shiftPositionsUp();
+		activeIdx === boxes.length - 1 ? activeIdx = 0 : activeIdx++;
+	} else if(remainingSteps < 0) {
+		shiftPositionsDown();
+		activeIdx === 0 ? activeIdx = boxes.length - 1 : activeIdx--;
+	}
+
+	if(remainingSteps > 1) {
+		disableControls = true;
+		setTimeout(() => animateClickSteps(remainingSteps - 1), menuTimeoutMS);
+	} else if(remainingSteps < -1) {
+		disableControls = true;
+		setTimeout(() => animateClickSteps(remainingSteps + 1), menuTimeoutMS);
+	} else {
+		disableControls = false;
+	}
+}
+
+function animate(e){
+	if(disableControls){
+		return
+	}
+	if(e.type === "click") {
+		debugger;
+		let selectedIdx = parseInt(e.target.getAttribute("data-idx"));
+		let steps = selectedIdx - activeIdx;
+		if(steps !== 0){
+			animateClickSteps(steps);
+		}
+	} else if(e.type === "keydown"){
+		if(e.key === "ArrowDown") {
+			shiftPositionsDown();
+			activeIdx === 0 ? activeIdx = boxes.length - 1 : activeIdx--;
+			disableControls = true;
+		} else if(e.key === "ArrowUp"){
+			shiftPositionsUp();
+			activeIdx === boxes.length - 1 ? activeIdx = 0 : activeIdx++;
+			disableControls = true;
+		}
+		setTimeout(()=>{disableControls = false},menuTimeoutMS);
+	}
+}
+
+document.addEventListener("keydown", animate)
